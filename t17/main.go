a191b3b72985f58a1a55ab69e73b3ce53a5ed47d
maypok86 <alex.mayshev.86@gmail.com>
2022-07-10 11:49:35 +0300

    add t17

---
+++ t17/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+/*
+Да кто эти ваши встроенные методы языка?..
+Это штуки из пакета sort или нужно прям алгос написать?
+*/
+
+func generateRandomSlice(size int) []int {
+	rand.Seed(time.Now().UnixNano())
+	slice := make([]int, size)
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(25)
+	}
+	return slice
+}
+
+func BinarySearch1(slice []int, x int) int {
+	return sort.Search(len(slice), func(i int) bool {
+		return slice[i] >= x
+	})
+}
+
+func BinarySearch2(slice []int, value int) int {
+	left := -1
+	right := len(slice)
+	for left < right-1 {
+		middle := left + (right-left)/2
+		if slice[middle] < value {
+			left = middle
+		} else {
+			right = middle
+		}
+	}
+	return right
+}
+
+func main() {
+	slice := generateRandomSlice(10)
+	sort.Ints(slice)
+	fmt.Println(slice)
+	x := slice[rand.Intn(len(slice))]
+	fmt.Println(x, BinarySearch1(slice, x))
+	fmt.Println(x, BinarySearch2(slice, x))
+}
