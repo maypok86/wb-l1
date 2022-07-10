8cce94474a0a47c63a6bc961d64e29f7d9a0f497
maypok86 <alex.mayshev.86@gmail.com>
2022-07-10 11:49:13 +0300

    add t15

---
+++ t15/main.go
@@ -0,0 +1,34 @@
+package main
+
+/*
+Проблемы две:
+
+1. Глобальная переменная justString
+Для исправления достаточно объявить её внутри функции
+P.S. в общем случае глобальные переменные - не всегда плохо, но здесь они не нужны
+
+2. Эта проблема, скорее всего, и подразумевалась
+string в Go - слайс байтов, а символы utf-8 кодируются несколькими байтами, так как одного байта не хватает для представления символа unicode
+Значит v[:100] сломается уже на кириллице. Выход: привести v к слайсу рун
+
+func someFunc() string {
+	v := createHugeString(1 << 10)
+	runes := []rune(v)
+	return string(runes[:100])
+}
+*/
+
+var justString string
+
+func createHugeString(_ int) string {
+	return "lalallaa"
+}
+
+func someFunc() {
+	v := createHugeString(1 << 10)
+	justString = v[:100]
+}
+
+func main() {
+	someFunc()
+}
