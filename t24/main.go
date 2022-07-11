352a968a1f0e1e35c95b9697366381610518c070
maypok86 <alex.mayshev.86@gmail.com>
2022-07-11 08:45:46 +0300

    add t24

---
+++ t24/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{
+		x: x,
+		y: y,
+	}
+}
+
+func (p Point) DistanceTo(other Point) float64 {
+	distX := p.x - other.x
+	distY := p.y - other.y
+	return math.Sqrt(distX*distX + distY*distY)
+}
+
+func main() {
+	p := NewPoint(1.25, 0.34)
+	fmt.Println("Distance:", p.DistanceTo(NewPoint(6.89, 7.01)))
+}
